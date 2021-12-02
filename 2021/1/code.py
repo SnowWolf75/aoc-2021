bc14919b37b77e6c00fb1e8fcb7f7aa8940ee883
Charles Wheeler <charles.wheeler@bigfishgames.com>
2021-12-01 17:29:15 -0800

    finish day 1

--- 2021/1/code.py
+++ 2021/1/code.py
@@ -3,15 +3,35 @@
 # Author = ?
 # Date = December 2021
 
-def
+print("Part One : "+ str(1387))
 
+def isGreater(old, new):
+    return new > old
+
+upCount = 0
+depths = []
+trio_a = 0
+trio_b = 0
 with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
-    input = input_file.read()
+    for line in input_file:
+        if len(depths) < 3:
+            depths.append(int(line))
+            continue
+
+        depths.append(int(line))
+        trio_a = sum(depths[0:3])
+        trio_b = sum(depths[1:4])
+        #print(depths)
+        #print(" %d => %d is " % (trio_a, trio_b), end='')
+        if isGreater(trio_a, trio_b):
+            upCount += 1
+            print("⬆️ ", end='')
+        else:
+            print("❌️ ", end='')
 
 
+        depths.pop(0)
 
-print("Part One : "+ str(None))
+print("")
 
-
-
-print("Part Two : "+ str(None))
+print("Part Two : "+ str(upCount))
